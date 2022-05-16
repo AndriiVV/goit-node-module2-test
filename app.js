8d0b7cbfacccab7b25d2b515c601dcc17766bee5
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-05-16 16:58:55 +0300

    My first http-server
    First http-server

---
+++ app.js
@@ -0,0 +1,19 @@
+// My first http-server
+
+// подключение модуля http
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// создание web-сервера с обработчиком запросов
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader = ("Content-Type", "text/plain");
+	res.end("Hello world!");
+});
+
+// запуск web-сервера
+server.listen(port, hostname, () => {
+	console.log(`Server is running at http://${hostname}:${port}/`);
+});
