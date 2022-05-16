bba4fea87fedc791eff24b89416b4a3ceb1787bb
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-05-16 17:23:01 +0300

    Update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader = ("Content-Type", "text/plain");
-	res.end("Hello world!");
+	res.end("Hello the best world!");
 });
 
 // запуск web-сервера
